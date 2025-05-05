f6d1b186f40855e1236e75dc7b266ca5f1b67c7c
Koishi Hsiao <9109222172@email.ncu.edu.cn>
2025-05-05 23:56:33 +0800

    fix: 修复在修复Platform时使用重定向导致的额外错误

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,12 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname)
 
+// // 强制重定向 PlatformConstants 引用
+// config.resolver.extraNodeModules = {
+//     ...config.resolver.extraNodeModules,
+//     'react-native/Libraries/Utilities/PlatformConstants': require.resolve(
+//       './platform-constants-shim.js'
+//     ),
+//   };
+
 module.exports = withNativeWind(config, { input: './global.css' })

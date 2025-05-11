33b0b5188b18d8cbea776251cd880d531aee2b09
Koishi Hsiao <9109222172@email.ncu.edu.cn>
2025-05-12 06:51:32 +0800

    build：页面枚举类新增

--- constants/pageInfo.ts
+++ constants/pageInfo.ts
@@ -1,9 +1,11 @@
+import FollowPage from "@/app/(home)/followPage";
 import FoundPage from "@/app/(home)/foundPage";
+import ProfilePage from "@/app/(home)/profilePage";
 
 export const PAGE_INFO={
     FoundPage:{
         title:"发现",
-        icon:"home",
+        icon:"found",
         component:FoundPage,
         headerShown:false,
         options:{
@@ -11,8 +13,26 @@ export const PAGE_INFO={
         }
     },
     MessagePage:{},
-    FollowPage:{},
+    FollowPage:{
+        title:"关注",
+        icon:"follow",
+        component:FollowPage,
+        headerShown:false,
+        options:{
+            headerShown:false,
+        }
+    },
+    ProfilePage:{
+        title:"个人",
+        icon:"profile",
+        component:ProfilePage,
+        headerShown:false,
+        options:{
+            headerShown:false,
+        }
+    },
     MinePage:{},
     MarkPage:{},
     LikePage:{},
+    
 }
